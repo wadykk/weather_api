bf0ca64bd24bd6e2ee51eaca0049d89128ada8af
wadykk <158468806+wadykk@users.noreply.github.com>
2024-05-02 14:40:58 +0000

    docs: changin comments in service brand

--- backend/src/services/brandService.ts
+++ backend/src/services/brandService.ts
@@ -2,85 +2,85 @@ import { faker } from "@faker-js/faker";
 import { storeBrandData } from "../helpers/helper.js";
 
 export const generateAppleBrandData = (): BrandData => {
-  // Generate random weather data
+  // Generate random phone brand data
   const generatedBrandData: BrandData = {
     model: "Apple",
     capacity: faker.number.int({ min: 4, max: 1028 }),
     colour: "Red",
   };
    storeBrandData(generatedBrandData).catch(console.error);
-  // Return weather data
+  // Return phone brand data
   return generatedBrandData;
 };
 
 export const generateSamsungBrandData = (): BrandData => {
-    // Generate random weather data
+    // Generate random phone brand data
     const generatedBrandData: BrandData = {
       model: "Samsung",
       capacity: faker.number.int({ min: 4, max: 1028 }),
       colour: "Black",
     };
      storeBrandData(generatedBrandData).catch(console.error);
-    // Return weather data
+    // Return phone brand data
     return generatedBrandData;
   };
 
   export const generateLenovoBrandData = (): BrandData => {
-    // Generate random weather data
+    // Generate random phone brand data
     const generatedBrandData: BrandData = {
       model: "Lenovo",
       capacity: faker.number.int({ min: 4, max: 1028 }),
       colour: "Yellow",
     };
      storeBrandData(generatedBrandData).catch(console.error);
-    // Return weather data
+    // Return phone brand data
     return generatedBrandData;
   };
 
   export const generateNokiaBrandData = (): BrandData => {
-    // Generate random weather data
+    // Generate random phone brand data
     const generatedBrandData: BrandData = {
       model: "Nokia",
       capacity: faker.number.int({ min: 4, max: 1028 }),
       colour: "Gray",
     };
      storeBrandData(generatedBrandData).catch(console.error);
-    // Return weather data
+    // Return phone brand data
     return generatedBrandData;
   };
 
   export const generateGoogleBrandData = (): BrandData => {
-    // Generate random weather data
+    // Generate random phone brand data
     const generatedBrandData: BrandData = {
       model: "Google",
       capacity: faker.number.int({ min: 4, max: 1028 }),
       colour: "White",
     };
      storeBrandData(generatedBrandData).catch(console.error);
-    // Return weather data
+    // Return phone brand data
     return generatedBrandData;
   };
 
   export const generateLGBrandData = (): BrandData => {
-    // Generate random weather data
+    // Generate random phone brand data
     const generatedBrandData: BrandData = {
       model: "LG",
       capacity: faker.number.int({ min: 4, max: 1028 }),
       colour: "Brown",
     };
      storeBrandData(generatedBrandData).catch(console.error);
-    // Return weather data
+    // Return phone brand data
     return generatedBrandData;
   };
 
   export const generateSonyBrandData = (): BrandData => {
-    // Generate random weather data
+    // Generate random phone brand data
     const generatedBrandData: BrandData = {
       model: "Sony",
       capacity: faker.number.int({ min: 4, max: 1028 }),
       colour: "Blue",
     };
      storeBrandData(generatedBrandData).catch(console.error);
-    // Return weather data
+    // Return phone brand data
     return generatedBrandData;
   };
